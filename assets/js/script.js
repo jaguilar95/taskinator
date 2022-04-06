1606eac6c85e845bdd79247335209fca4a48b65f
Leonardo <josue.aguilar1995@gmail.com>
2022-04-05 22:18:17 -0500

    added empty form validation and form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ var taskFormHandler = function (event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("Please enter a task name and a task type!");
+    return false;
+  }
+
+  formEl.reset();
+
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
